3f2b0ed67a46ecac8188dec3605f9f197b05e436
Lloyd <110196181+Acn6S@users.noreply.github.com>
2025-10-17 11:15:21 -0500

    UIE update

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -7,7 +7,7 @@ from typing import Callable, Dict, List, Optional, Set
 
 import pandas as pd
 import yaml
-from pydantic import Field, field_validator
+from pydantic import Field, field_validator 
 
 from hummingbot.client import settings
 from hummingbot.client.config.config_data_types import BaseClientModel
