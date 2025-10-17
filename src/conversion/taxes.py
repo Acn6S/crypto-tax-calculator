5c5e28b921e8032339066e3f3e7ca8e646f9cff2
Lloyd <110196181+Acn6S@users.noreply.github.com>
2025-10-17 11:12:58 -0500

    UIE update

--- src/conversion/taxes.py
+++ src/conversion/taxes.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from pandas import DataFrame
 
-from freqtrade.exceptions import StrategyError
+from freqtrade.exceptions import StrategyError 
 
 
 logger = logging.getLogger(__name__)
