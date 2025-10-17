036b34a959101cecb7d3c4c94e105fe93d7aa763
Lloyd <110196181+Acn6S@users.noreply.github.com>
2025-10-17 11:20:10 -0500

    correct handling of negative balances

--- src/pool.py
+++ src/pool.py
@@ -5,7 +5,7 @@ import pytest
 from additional_tests.historical_backend_tests import clickhouse_client
 import octobot.community.history_backend.clickhouse_historical_backend_client as clickhouse_historical_backend_client
 
-import octobot_commons.enums as commons_enums
+import octobot_commons.enums as commons_enums 
 
 
 # All test coroutines will be treated as marked.
